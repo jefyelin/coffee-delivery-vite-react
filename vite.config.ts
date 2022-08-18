71bd324143001e76be120f463fe92d13d8bd9ec7
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-18 15:40:02 -0300

    adjust setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
+    setupFiles: './src/utils/tests/setup',
     css: true,
   },
 })
