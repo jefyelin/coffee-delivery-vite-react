d4c7681fa8a630015c636413994e6065b8f13e02
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-18 15:40:13 -0300

    adjust theme type

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -1,24 +1,26 @@
-export type Theme = {
-  colors: {
-    base: {
-      title: string
-      subtitle: string
-      text: string
-      label: string
-      hover: string
-      button: string
-      input: string
-      card: string
-      background: string
-      white: string
-    }
-    product: {
-      yellow: string
-      yellowLight: string
-      yellowDark: string
-      purple: string
-      purpleLight: string
-      purpleDark: string
-    }
+type ThemeColor = {
+  base: {
+    title: string
+    subtitle: string
+    text: string
+    label: string
+    hover: string
+    button: string
+    input: string
+    card: string
+    background: string
+    white: string
   }
+  product: {
+    yellow: string
+    yellowLight: string
+    yellowDark: string
+    purple: string
+    purpleLight: string
+    purpleDark: string
+  }
+}
+
+export type Theme = {
+  color: ThemeColor
 }
