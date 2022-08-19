19a35ad6e7b2cd3814de734b7f7d2efd89ad1d33
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-19 19:46:12 -0300

    adjust logo url

--- src/components/navbar/components/logo/logo.tsx
+++ src/components/navbar/components/logo/logo.tsx
@@ -3,7 +3,7 @@ import logoImageUrl from '../../../../assets/logo.svg'
 
 export function Logo() {
   return (
-    <Link to='/home'>
+    <Link to='/'>
       <img
         src={logoImageUrl}
         alt={`Logo da Coffee Delivery com a ilustração de um copo roxo com um foguete branco 
