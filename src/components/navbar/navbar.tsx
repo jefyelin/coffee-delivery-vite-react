9807a97e4da933bdead49ca9285e8e90477fb3d0
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-22 19:55:51 -0300

    adjust navbar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Cart } from './components/cart'
 import { Location } from './components/location'
 import { Logo } from './components/logo'
@@ -9,7 +10,9 @@ export function Navbar() {
       <Logo />
       <LocationAndCartContainer>
         <Location />
-        <Cart />
+        <Link to='/checkout'>
+          <Cart />
+        </Link>
       </LocationAndCartContainer>
     </Container>
   )
