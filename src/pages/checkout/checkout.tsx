03944142bd78ed668e66aa2d00e18c4dfa7f90c0
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-22 19:56:15 -0300

    add checkout page

---
+++ src/pages/checkout/checkout.tsx
@@ -0,0 +1,9 @@
+import { Title } from '../../components/typography/title'
+
+export function Checkout() {
+  return (
+    <Title variant='h1' color='title'>
+      Checkout Page
+    </Title>
+  )
+}
