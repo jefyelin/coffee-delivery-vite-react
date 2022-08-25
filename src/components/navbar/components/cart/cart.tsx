30a67c8d0f2d71a29a12b6767436fb440ed54938
Jean Arruda <jean.arruda@thoughtworks.com>
2022-08-24 22:23:02 -0300

    adjust navbar cart component to useCheckout hook

--- src/components/navbar/components/cart/cart.tsx
+++ src/components/navbar/components/cart/cart.tsx
@@ -1,16 +1,23 @@
 import { ShoppingCart } from 'phosphor-react'
+import { useCheckout } from '../../../../hooks/checkout'
 import { Text } from '../../../typography/text'
 import { Button, ItemsCounter } from './cart.styles'
 
 export function Cart() {
+  const { getTheQuantityOfTheTotalItems } = useCheckout()
+
+  const quantityOfTheTotalItems = getTheQuantityOfTheTotalItems()
+
   return (
     <Button type='button' aria-describedby='cart-items-counter'>
       <ShoppingCart size={22} weight='fill' alt='Ícone de um carrinho de compras' />
-      <ItemsCounter id='cart-items-counter'>
-        <Text variant='boldS' color='white' aria-label='3 itens no carrinho de compras'>
-          3
-        </Text>
-      </ItemsCounter>
+      {quantityOfTheTotalItems > 0 && (
+        <ItemsCounter id='cart-items-counter'>
+          <Text variant='boldS' color='white' aria-label='3 itens no carrinho de compras'>
+            {quantityOfTheTotalItems}
+          </Text>
+        </ItemsCounter>
+      )}
     </Button>
   )
 }
